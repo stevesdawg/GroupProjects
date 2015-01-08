b164ed5636a4d7077950b5f1effac34c6acd29d3
Shrivathsav Seshan <shrivathsav.seshan@gmail.com>
2015-01-08 15:21:01 -0500

    added shared resource to client input stream

--- Chat/src/Chat/ClientInput.java
+++ Chat/src/Chat/ClientInput.java
@@ -4,28 +4,37 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class ClientInput implements Runnable{
 	private static Socket sock;
 	private static Scanner scan;
+	private ArrayBlockingQueue<String> incomingMessages;
 	
-	public ClientInput() throws UnknownHostException, IOException{
+	public ClientInput(ArrayBlockingQueue<String> in) throws UnknownHostException, IOException{
 		sock=new Socket("10.5.101.127",80);
 		scan=new Scanner(sock.getInputStream());
+		incomingMessages = in;
 	}
 	
 	public void run(){
 		while(true)
-		printOut();
+			try {
+				printOut();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 	}
 	
 	public static Socket getSocket(){
 		return sock;
 	}
 	
-	public void printOut(){
+	public void printOut() throws InterruptedException{
 		while(scan.hasNextLine()){
-			System.out.println(scan.nextLine());
+			String message = scan.nextLine();
+			System.out.println(message);
+			incomingMessages.put(message);
 		}
 	}
 
